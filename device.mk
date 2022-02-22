ab617510f8862cd074425e3956ea0fdd151faf1a
sarthakroy2002 <sarthakroy2002@users.noreply.github.com>
2022-02-23 05:57:35 +0700

    RMX1941: Cleanup prebuilts and build bunch required blobs
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Co-authored-by: karthick111 <karthickrko61@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I61f2beb416af27fe149c87f0036c9a2169a6d11a

--- device.mk
+++ device.mk
@@ -42,17 +42,24 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0.vendor \
-    android.hardware.audio.common@5.0-util.vendor \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.2.vendor \
+    android.hardware.soundtrigger@2.2.vendor
+	
+PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
+    audio_policy.stub
+	
+PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libtinycompress \
-    libtinyxml
+    libtinyxml \
+    libalsautils \
+    libnbaio_mono
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
@@ -60,15 +67,15 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_em.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_em.xml \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml \
-    $(DEVICE_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(DEVICE_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+    $(DEVICE_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -88,6 +95,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor
+	
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+	
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlay
 
 # Dependencies of kpoc_charger
 PRODUCT_PACKAGES += \
@@ -109,8 +122,14 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1
-
+	
+PRODUCT_PACKAGES += \
+    libvulkan
+	
 # DRM
+PRODUCT_PACKAGES += \
+    libmockdrmcryptoplugin
+
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
@@ -118,6 +137,14 @@ PRODUCT_PACKAGES += \
 # FSTAB
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_SYSTEM)/etc/fstab.mt6765
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+	
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
@@ -131,6 +158,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.0 \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
@@ -191,6 +219,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
+	
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
 
 # Neutral Networks
 PRODUCT_PACKAGES += \
@@ -328,7 +360,12 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0.vendor
+    android.hardware.sensors@2.0.vendor \
+    android.hardware.sensors@2.0-impl \
+    android.hardware.sensors@2.0-service \
+	
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
 
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
@@ -342,6 +379,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Symbols
 #PRODUCT_PACKAGES += \
 #    libshim_vtservice
+
+# Text classifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_hash.vendor
 
 # Tethering
 PRODUCT_PACKAGES += \
