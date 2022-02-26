a6c7dcba8cb9cf6e9bfee949cdf2be49f01b8e1b
AIV-97 <AIV-97@users.noreply.github.com>
2022-02-27 01:45:45 +0700

    RMX1941: DOnt't build libvulkan
    Realme C2 doesn't support vulkan , so no need build vulkan libs
    
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>
    Change-Id: I213e7d695d68c62f66551628447d4a2efcf59e9a

--- device.mk
+++ device.mk
@@ -123,9 +123,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1
 	
-PRODUCT_PACKAGES += \
-    libvulkan
-	
 # DRM
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
